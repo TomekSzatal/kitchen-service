ba3d0378068ec14fc59c72456a1fa848a5f054b2
TomekSzatal <szatalowicztomek@gmail.com>
2026-06-04 11:33:37 +0200

    Rename Kichen to Kitchen

--- kitchen_service/urls.py
+++ kitchen_service/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for kichen_service project.
+URL configuration for kitchen_service project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.0/topics/http/urls/
