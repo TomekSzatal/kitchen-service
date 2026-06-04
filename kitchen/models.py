41c4a7d58f49c28bf464d34e2180aab5d145d471
TomekSzatal <szatalowicztomek@gmail.com>
2026-06-04 12:06:59 +0200

    Add new models in kitchen Cook, Dish and Dishtype

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "cook"
+        verbose_name_plural = "cooks"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE)
+    cooks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cooks")
+
+    def __str__(self):
+        return self.name
