4fa6a38608e577d4bce7041695994a10eaa67e9a
TomekSzatal <szatalowicztomek@gmail.com>
2026-06-08 12:22:55 +0200

    update settings & requirements.txt

--- kitchen_service/settings.py
+++ kitchen_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-85&^n$%9%8y7msdw#c^ck(kjjr7=)fcj@4y=$b%u#ali$g9ah+"
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-85&^n$%9%8y7msdw#c^ck(kjjr7=)fcj@4y=$b%u#ali$g9ah+")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") == "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
